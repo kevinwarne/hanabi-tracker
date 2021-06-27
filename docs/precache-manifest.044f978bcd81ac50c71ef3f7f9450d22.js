f7b33f76a670fa053ab7d9526aabe527ae19d993
Kevin Warne <kevinwarne@Kevins-MacBook-Pro.local>
2021-06-26 20:30:52 -0600

    enable pwa

---
+++ docs/precache-manifest.044f978bcd81ac50c71ef3f7f9450d22.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e90d6fcf60962f1b3dc",
+    "url": "/hanabi-tracker/css/app.9f1df5fa.css"
+  },
+  {
+    "revision": "7fd8ad6e53996643bf37",
+    "url": "/hanabi-tracker/css/chunk-vendors.d5ea7faa.css"
+  },
+  {
+    "revision": "776f2b7afd866ecf4a70d32c270cf531",
+    "url": "/hanabi-tracker/img/blue.776f2b7a.png"
+  },
+  {
+    "revision": "8e42c6db5abd976790d2b2b614580e5b",
+    "url": "/hanabi-tracker/img/green.8e42c6db.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/hanabi-tracker/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "04754e75852ff2c2452aab1c073c1736",
+    "url": "/hanabi-tracker/img/multicolor.04754e75.png"
+  },
+  {
+    "revision": "16bb738253b8b377e5eabf14bff48a36",
+    "url": "/hanabi-tracker/img/red.16bb7382.png"
+  },
+  {
+    "revision": "8f351761b1519abe4eb1b0bf0a7cf8ad",
+    "url": "/hanabi-tracker/img/white.8f351761.png"
+  },
+  {
+    "revision": "22fd965a93b1ad6575bbcf00f5f38447",
+    "url": "/hanabi-tracker/img/yellow.22fd965a.png"
+  },
+  {
+    "revision": "f0bc524e3a325d1f85f7d97ee95032a5",
+    "url": "/hanabi-tracker/index.html"
+  },
+  {
+    "revision": "0e90d6fcf60962f1b3dc",
+    "url": "/hanabi-tracker/js/app.2e529b81.js"
+  },
+  {
+    "revision": "7fd8ad6e53996643bf37",
+    "url": "/hanabi-tracker/js/chunk-vendors.c6ec1c51.js"
+  },
+  {
+    "revision": "958e49c5194e499ad855063d3d43bf84",
+    "url": "/hanabi-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hanabi-tracker/robots.txt"
+  }
+]);
